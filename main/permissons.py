8bd144e07486977e49a9e7a48bea01d59f9c54d4
Vika Chupilko <chupilko.vika2015@gmail.com>
2020-11-01 10:21:09 +0300

    removed excess coments

--- main/permissons.py
+++ main/permissons.py
@@ -5,13 +5,7 @@ from rest_framework.exceptions import PermissionDenied
 
 
 class TeacherPermission(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         if User2.objects.filter(user=request.user).exists():
             if Teacher.objects.filter(user=request.user.user2).exists():
                 return True
@@ -20,8 +14,6 @@ class TeacherPermission(permissions.BasePermission):
 
 class PupilPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         if User2.objects.filter(user=request.user).exists():
             if Pupil.objects.filter(user=request.user.user2).exists():
                 return True
